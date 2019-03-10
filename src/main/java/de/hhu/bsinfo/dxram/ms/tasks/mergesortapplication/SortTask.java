b0d85b37a2cf54ef1f4d6ed8dfebccd60cd9ca1b
jusch196 <jusch196@hhu.de>
2019-03-10 16:17:12 +0100

    cleaned code

--- src/main/java/de/hhu/bsinfo/dxram/ms/tasks/mergesortapplication/SortTask.java
+++ src/main/java/de/hhu/bsinfo/dxram/ms/tasks/mergesortapplication/SortTask.java
@@ -34,8 +34,6 @@ public class SortTask implements Task {
 
     @Override
     public int execute(TaskContext p_ctx) throws IOException {
-        System.out.println("Start: " + System.nanoTime());
-
         chunkService = p_ctx.getDXRAMServiceAccessor().getService(ChunkService.class);
         NameserviceService nameService = p_ctx.getDXRAMServiceAccessor().getService(NameserviceService.class);
 
